0ebffb88d161f35e89886ceb48354bb2f5f81634
Cyuan <dec880126@gmail.com>
2021-10-13 23:18:10 +0800

    建立檔案

--- Homework/HW#03.py
+++ Homework/HW#03.py
@@ -1,5 +1,6 @@
 """
- # Evolutionary Computation HW #03
+ # Evolutionary Computation HW #02
+
  - Student ID: 7110064490
  - Name: Huang Sin-Cyuan(黃新荃)
  - Email: dec880126@icloud.com
---
+++ Homework/HW#04.py
@@ -0,0 +1,7 @@
+"""
+ # Evolutionary Computation HW #02
+
+ - Student ID: 7110064490
+ - Name: Huang Sin-Cyuan(黃新荃)
+ - Email: dec880126@icloud.com
+"""
